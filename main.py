b241e254a61ac7e95b6c8c58603be0e3ce4c4598
LatekVon <latkaignacy@gmail.com>
2022-09-21 22:14:45 +0200

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyautogui
+import graphics
+
+# create a window, grab output by
+# (FPS) hiding the cursor, moving it to center of the window and registering any deviations.
+# (GUI) cursor visible and movable + some GUI library
+if __name__ == '__main__':
+    print(":^)")
