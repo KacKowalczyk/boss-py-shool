6da7bcc2c09d7aabba792984f01d29d356784e07
LatekVon <latkaignacy@gmail.com>
2022-10-01 02:32:47 +0200

    - Went through a couple of iterations of deleting and adding the same pyPI deps. - Implemented global deltaTime (not used everywhere yet) - Extended Player's methods (they should cover most of possible actions now) - Added a basic weapon model - Successfully implemented weapon swing - Screen clearing implemented (couldn't find it anywhere in the official docs)

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
+# standard imports
+import time
 import math
+
+# install those packages
 import pyautogui as gui
+
 import graphics
 from graphics import *
+
+
+timeDelta = 1.
 
 
 class Enemy:
@@ -37,16 +45,40 @@ class Enemy:
 
 
 class Player:
-    x = y = 0
-    rotX = rotY = rotZ = 0
+    x = y = 0  # will be derived from rotation (and the other way around in case of dodge move)
+    rotationSpeed = 3 * math.radians(1)  # in degrees
+
+    # rotO - relative rotation, rest - global rotation (possibly irrelevant) (rotO = rotZ)
+    rotO = rotX = rotY = rotZ = 0
+    moveDelta = 0
     charge = 100
     health = 3
 
-    coolDownTime = 1.
-    coolDown = 0.
+    coolDownTime = 1.  # in seconds
+    coolDown = 0.  # counting down from whatever it's set to
+    shootNextTime = 0
+
+    # current coords -> new rotation info
+    def update_rotation(self, e: Enemy):
+        print("p rotation")
+
+    # current rotation -> new coords info
+    def update_position(self, e: Enemy):
+        print("p position")
+
+    # direction is really either negative or positive
+    def move(self, e: Enemy, direction: int):
+        print("p move")
+
+    def shoot(self, e: Enemy):
+        if e.midAnimation == 1:
+            return
+
+        self.coolDown = self.coolDownTime
+        self.charge -= 40
+        self.shootNextTime = 1
 
     def update(self, e: Enemy):
-        print("p update")
 
         if e.midAnimation == 1:
             (x, y) = e.find_closest_point(self.x, self.y)
@@ -67,16 +99,18 @@ class Camera:
     # overlay variables
     swingIter = 0  # weapon's swing sinusoid's iterator
 
+    weaponModel = "w_rifle.png"
+
     def update(self, p: Player):
         print("c update")
 
-    def applyCameraMatrix(self, window: GraphWin):
+    def apply_camera_matrix(self, win: GraphWin):
         print("c cam matrix")
 
-    def applyRotationMatrix(self, obj: tuple, rad: float):
+    def apply_rotation_matrix(self, obj: tuple, rad: float):
         print("c rot matrix")
 
-    def draw_overlay(self, p: Player):
+    def draw_overlay(self, p: Player, win: GraphWin):
         swing_x_pre_multiplier = math.pi / 3
         swing_x_post_multiplier = 1
 
@@ -86,14 +120,23 @@ class Camera:
         swing_iter_steps = 0.1
         swing_iter_cap = math.pi * 10  # putting a big number here is a hacky way to avoid animation jumps
 
+        movement_multiplier = 100
+
         swing_x_offset = math.sin(self.swingIter * swing_x_pre_multiplier) * swing_x_post_multiplier
         swing_y_offset = math.cos(self.swingIter * swing_y_pre_multiplier) * swing_y_post_multiplier
 
-        self.swingIter += swing_iter_steps
+        base_pos_x = win.getWidth() / 2
+        base_pos_y = win.getHeight() - 100
+
+        img = Image(Point(base_pos_x + swing_x_offset * movement_multiplier,
+                          base_pos_y + swing_y_offset * movement_multiplier),
+                          "w_rifle.png")
+
+        img.draw(win)
+
+        self.swingIter += swing_iter_steps * p.moveDelta  # time delta already applied
         if self.swingIter > swing_iter_cap:
             self.swingIter = 0
-
-        print("c overlay")
 
     # draws floor tile-by-tile, draw_floor_wire will be a simpler but more crude version of this
     def draw_floor_tiles(self):
@@ -105,21 +148,22 @@ class Camera:
 
         square_dim = 20
 
-        x_increment = (1, 0)
-        y_increment = (0, 1)
+        x_increment = [1, 0]
+        y_increment = [0, 1]
 
         x_iter = 0
         y_iter = 0
 
-        x_base = (0, 0)  # main iterated position
-        y_base = (0, 0)  # cached y position for ease of resetting position and moving on to a new row
-
         color_iter = 0
         color_adjust = 1
 
-        while y_iter < 20:
-            while x_iter < 20:
-                print("c loop")
+        y_base = [0, 0]  # cached y position for ease of resetting position and moving on to a new row
+
+        while y_iter < square_dim:
+
+            x_base = y_base  # main iterated position
+
+            while x_iter < square_dim:
 
                 color_iter += 1
                 x_base += x_increment
@@ -133,12 +177,9 @@ class Camera:
                 color_adjust = 1
 
             y_base += y_increment
-            x_base = y_base
 
             x_iter = 0
             y_iter += 1
-
-        print("c floor")
 
     def draw_enemy(self, e: Enemy):
         print("c draw")
@@ -151,22 +192,31 @@ if __name__ == '__main__':
 
     print("\n:^O\n")
 
-    window = GraphWin('Don\'t stop me now, kek.', 300, 300)
+    window = GraphWin('Don\'t stop me now, kek.', 800, 600, autoflush=False)
     enemy: Enemy = Enemy()
     camera: Camera = Camera()
     player: Player = Player()
 
     print("\n:^)\n")
 
+    # window closing magically fixed itself :DDD
     while window.isOpen():
+        start_time = time.time()
 
         enemy.update()
         player.update(enemy)
         camera.update(player)
 
-        camera.draw_floor()
+        # very slow, sufficient for now
+        for item in window.items[:]:
+            item.undraw()
+
+        camera.draw_floor_tiles()
         camera.draw_enemy(enemy)
-        camera.draw_overlay(player)
+        camera.draw_overlay(player, window)
+
         window.update()
+
+        timeDelta = time.time() - start_time
 
     print("\n:^D")
